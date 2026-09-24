c8462a2ecd04c396fb3d27cddd288f48043b4e58
Mario Loueke <m.loueke@rubika-edu.ca>
2026-09-23 20:55:05 -0400

    Update BendyMario.py

--- BobosTeam/personal/LouekeMArio/BendyMario.py
+++ BobosTeam/personal/LouekeMArio/BendyMario.py
@@ -2,12 +2,21 @@ import pymel.core as pm
 
 
 def curve_by_points(*points):
+    point_data=[]
     for each in points:
-        print(each)
+        point_data.append(each.translate.get())
+        print(each.translate.get())
+
+        #point_data=[each.translate.get() for each in points]
+    new_curve= pm.curve(point=point_data)
+    pm.rebuildCurve(new_curve,
+                    rebuildType=0,
+                    keepRange=0,
+                    replaceOriginal=True,
+                    constructionHistory=False,)
 
 
 selection = pm.ls(selection=True)
 
-print(selection, selection.__class__)
 
 curve_by_points(*selection)
