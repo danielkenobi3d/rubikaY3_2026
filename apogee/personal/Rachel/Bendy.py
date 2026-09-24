61a184a5aaad7cb8b9a910c2528ed263b6c55e1d
Rachel <wendymarvel871@gmail.com>
2026-09-23 20:56:21 -0400

    Updtae Bendy

--- apogee/personal/Rachel/Bendy.py
+++ apogee/personal/Rachel/Bendy.py
@@ -2,9 +2,14 @@ import pymel.core as pm
 
 
 def curve_by_points(*points):
+    point_data = []
     # print (points, points.__class__)
     for each in points :
-        print (each)
+        print (each.translate.get())
+    print (point_data)
+    point_data = [each.translate.get() for each in points] #in one ligne
+    new_Curve = pm.curve(editPoint =point_data)
+    pm.rebuildCurve(new_Curve, rebuildType = 0, keepRange= 0, constructionHistory = True, replaceOriginal = False)
 
 
 selection = pm.ls(selection=True)
